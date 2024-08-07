52b36c5627fa42ab5c53090ca7566859191b7b23
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-07 11:41:38 +0300

    chore: added function that returns a promise reject

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+// export default function uploadPhoto(filename) {
+//   return new Promise((resolve, reject) => {
+//     reject(`${filename} cannot be processed`);
+//   });
+// }
+//
+export default function uploadPhoto(filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}

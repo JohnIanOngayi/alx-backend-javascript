c93405c3de253df9eb308aa7a1c30f64196a30a6
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-07 11:47:03 +0300

    chore: added function that handles multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignUp(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const p1 = signUpUser(firstName, lastName);
+  const p2 = uploadPhoto(fileName);
+  const data = await Promise.allSettled([p1, p2]).then((results) =>
+    results.map((result) => {
+      if (result.status !== 'fulfilled') {
+        return {
+          status: 'rejected',
+          value: result.reason.toString(),
+        };
+      }
+      return result;
+    }),
+  );
+
+  return Promise.resolve(data);
+}

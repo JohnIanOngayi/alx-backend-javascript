8aef6bd86d03b1a194e3a87dc0b2a4d74b1004c5
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-07 11:14:37 +0300

    chore: function that handles multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignUp() {
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+
+  return Promise.all([p1, p2])
+    .then((data) => {
+      const result = Object.assign(...data);
+      console.log(`${result.body} ${result.firstName} ${result.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}

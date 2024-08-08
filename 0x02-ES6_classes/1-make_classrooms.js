11973425e8c31347b5d6f9a528c4ba62fa5399df
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-08 13:19:20 +0300

    refactor: added elements directly into array

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,6 @@
 import Classroom from './0-classroom';
 
 export default function initializeRooms() {
-  const classrooms = [];
-  classrooms.push(new Classroom(19), new Classroom(20), new Classroom(34));
+  const classrooms = [new Classroom(19), new Classroom(20), new Classroom(34)];
   return classrooms;
 }

9582611411c516bafed68b5a4f49dd4e42d19bae
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-08 12:50:31 +0300

    chore: added function that creates Classroom's

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+import Classroom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const classrooms = [];
+  classrooms.push(new Classroom(19), new Classroom(20), new Classroom(34));
+  return classrooms;
+}

6f1e3acfd561bd5b40485f3d84a35f7bc7798ae8
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-08 17:42:15 +0300

    chore: implemented class SkyHighBuilding that inherits from class Building

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value === 'number') {
+      this._floors = value;
+    } else {
+      throw new TypeError('floors must be a number');
+    }
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}

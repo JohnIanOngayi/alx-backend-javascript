0d82cd32b44d6100cd1a6b9e8d0504763cb030f0
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-08 12:48:49 +0300

    chore: implemented class Classroom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+/**
+ * Class defines a classroom
+ */
+export default class Classroom {
+  /**
+   * instaniates a Classroom object
+   * @param {number} maxStudentsSize: max no of students allowed in Classroom
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}

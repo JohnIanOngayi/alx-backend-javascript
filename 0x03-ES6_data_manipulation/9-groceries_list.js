5d9090f398db96a35996904b5253ccc61f1b09fa
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 19:16:31 +0300

    chore: implemented function that returns a map

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+/**
+ * function returns map from list
+ *
+ * @returns {Map} returns a map
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}

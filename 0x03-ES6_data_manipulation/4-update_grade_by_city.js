90427a3efda223c705cdc8425cfa23d06f5db978
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:34:37 +0300

    chore: implemented a function that updates grades for students of said location

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+/**
+ * function returns an array of students for city with their new grade
+ * @param {Object} listStudents: list of student objects
+ * @param {String} city: city to match
+ * @param {Object} newGrades: list of new grades
+ * @returns{list} array of students with new grade
+ */
+export default function (listStudents, city, newGrades) {
+  return listStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find(
+        (grade) => grade.studentId === student.id,
+      );
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}

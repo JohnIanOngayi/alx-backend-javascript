b0b9acd6838f230fb77de9627c832b6177dd448c
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 19:13:53 +0300

    chore: implemented function that slices and concatenates elements of an array

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+/**
+ * function returns a string of all set values that start with a specifi string
+ *
+ * @returns{String} string of all set values starting with startString
+ */
+export default function cleanSet(set, startString) {
+  const myStrings = []; // stores the values that match the condition
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      myStrings.push(s.slice(startString.length));
+    }
+  });
+  return myStrings.join('-');
+}

6585436b1ac4a99ae44f682ed571469cfbbb46a3
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:14:03 +0300

    chore: implemented function that returns array of objs whole location match specified string

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+/**
+ * function fetches objects by locaton
+ *
+ * @param {listStudents} list of student objects
+ * @param {city} city to match
+ * @returns {Array} an array of objects who are located in a specific city.
+ */
+export default function getStudentsByLocation(listStudents, city) {
+  if (!Array.isArray(listStudents) || typeof city !== 'string') {
+    return [];
+  }
+  const studentsByLoc = listStudents.filter((value) => value.location === city);
+  return studentsByLoc;
+}

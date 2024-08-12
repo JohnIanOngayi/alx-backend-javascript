ddedfe34f3393b744c0c55c616f47f88eb2211f6
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:11:28 +0300

    chore: implemented function that returns array of id's from list of objects

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+/**
+ * function returns an array of id's from an array of objects
+ *
+ * @returns (list) List of student id's
+ */
+export default function getListStudentIds(studentObjs) {
+  if (!Array.isArray(studentObjs)) {
+    return [];
+  }
+  const studentIds = studentObjs.map((value) => value.id);
+  return studentIds;
+}

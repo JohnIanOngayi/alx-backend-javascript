3fde297e0b215facba4c0271c1d2f52dff2628c6
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:08:15 +0300

    chore: implemented function that returns array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+/**
+ * function returns an array of objects
+ *
+ * @returns (List) List of objecs`
+ */
+export default function getListStudents() {
+  const User1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const User2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const User3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [User1, User2, User3];
+}

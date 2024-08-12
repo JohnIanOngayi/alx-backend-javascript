51ffd10e65b4d4e408d84a14218c2022dc6f0d15
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:44:51 +0300

    chore: implemented function that returns set from an array

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+/**
+ * function retunrs a set from an array
+ *
+ * @param {Array} array : array to be converted to set
+ * @returns {Set} returns set from array
+ */
+export default function (array) {
+  if (!Array.isArray(array)) {
+    throw new Error('Argument must be an array');
+  }
+  return new Set(array);
+}

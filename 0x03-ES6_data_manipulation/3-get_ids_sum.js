98d330022000da7c6e777cff9201ca4a3238cd2c
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:28:20 +0300

    chore: implemented function that uses reduce to return sum of student ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+/**
+ * function returns sum of all ids
+ *
+ * @returns {number} sum of all student ids
+ */
+export default function getStudentIdsSum(ListStudents) {
+  if (!Array.isArray(ListStudents)) {
+    throw new TypeError('ListStudents must be an array');
+  }
+  return ListStudents.reduce((total, item) => {
+    if (!Number.isInteger(item.id)) {
+      throw new TypeError('All student id must be integers');
+    }
+    return total + item.id;
+  }, 0);
+}

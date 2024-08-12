36080ae2aa1311222952b9dfa3a8483077a0de02
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:50:19 +0300

    chore: implemented function that checks whether array is subset of set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+/**
+ * functions returns a boolean if elements in array exist in set
+ *
+ * @param {Set} set: set to be searched in
+ * @param {Array} array: array of values to be searched for
+ * @returns {boolean} returns if union exists between array and set
+ */
+export default function (set, array) {
+  return array.every((element) => set.has(element));
+}

28ef053990f4d3b91311a2c0ac7cc31d916702bd
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 19:20:19 +0300

    chore: implemented function that modifies specified values of a map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+/**
+ * function returns an updated map
+ *
+ * @param {Map} map: original map to be iterated
+ * @returns {Map} map for all items with initial quantity 1
+ */
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    map.forEach((value, key) => {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    });
+    return map;
+  }
+  throw new Error('Cannot process');
+}

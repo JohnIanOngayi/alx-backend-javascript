d6182850e79809c91282e53db336af88393be336
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-12 18:38:37 +0300

    chore: implemented function that returns ArrayBuffer with int8 at specified position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+/**
+ * function returns new ArrayBuffer with int8 value at specified position
+ *
+ * @returns {ArrayBuffer} returns new ArrayBuffer
+ */
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8view = new DataView(buffer);
+  int8view.setInt8(position, value);
+  return int8view;
+}

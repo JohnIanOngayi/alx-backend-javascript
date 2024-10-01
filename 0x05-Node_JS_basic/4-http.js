d036d62d907367ad662c7eba0ce992cf52064ec2
JohnIanOngayi <ongayijohnian@gmail.com>
2024-10-01 08:58:45 +0300

    task: added function that creates HTTPServer

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const http = require('node:http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(1245);
+
+module.exports = app;
